660b98da3d77c986f72cdbb531a6732a540334a8
Maxim Sizonenko <msize@ya.ru>
2022-08-14 12:32:15 +0200

    Add human vs machine mode

--- game/human_vs_human/human_vs_human.go
+++ game/human_vs_human/human_vs_human.go
@@ -1,10 +1,10 @@
 package human_vs_human
 
 import (
-	"crypto/rand"
 	"fmt"
 
 	"github.com/numbers-game/consts"
+	"github.com/numbers-game/game/common"
 	"github.com/numbers-game/permutations"
 	"github.com/numbers-game/score"
 	"github.com/numbers-game/types"
@@ -14,15 +14,14 @@ func Run() {
 	fmt.Println("Human vs Human mode")
 	numbers := permutations.Generate()
 	fmt.Print("Your hidden number: ")
-	hidden := fetchRundomNumber(numbers)
+	hidden := common.FetchRundomNumber(numbers)
 	fmt.Println(hidden)
-	youFirst := youFirst()
+	youFirst := common.YouFirst()
 	var yourScores types.Scores
 	var yourScore, opponentScore types.Score
 	for i := 2; !score.Win(yourScore) || !score.Win(opponentScore); i++ {
 		if i%2 == 0 {
 			fmt.Printf("========== %d ==========\n", i/2)
-			fmt.Println(!score.Win(yourScore) || !score.Win(opponentScore))
 		}
 		if i%2 == 0 && youFirst || i%2 != 0 && !youFirst {
 			yourScores = yourTurn(numbers, yourScores)
@@ -42,7 +41,7 @@ func yourTurn(numbers types.Numbers, yourScores types.Scores) types.Scores {
 		return yourScores
 	}
 	fmt.Print("Your turn: ")
-	calledNumber := fetchRundomNumber(numbers)
+	calledNumber := common.FetchRundomNumber(numbers)
 	fmt.Print(calledNumber)
 	fmt.Printf(" (%d)\n", leftNumbers)
 	fmt.Print("Your score: ")
@@ -50,45 +49,13 @@ func yourTurn(numbers types.Numbers, yourScores types.Scores) types.Scores {
 		fmt.Println(" 4 4")
 		return score.AppendScore(yourScores, types.Score{Number: calledNumber, Guessed: consts.Digits, Postions: consts.Digits})
 	}
-	return score.AppendScore(yourScores, scanScore(calledNumber))
+	return score.AppendScore(yourScores, common.ScanScore(calledNumber))
 }
 
 func opponentTurn(hidden types.Number) types.Score {
 	fmt.Print("Opponent turn: ")
-	opponent := scanNumber()
+	opponent := common.ScanNumber()
 	opponentScore := score.Calc(opponent, hidden)
 	fmt.Printf("Opponent score: %d-%d\n", opponentScore.Guessed, opponentScore.Postions)
 	return opponentScore
-}
-
-func fetchRundomNumber(numbers types.Numbers) types.Number {
-	return numbers[randInt(len(numbers))]
-}
-
-func randInt(len int) uint {
-	RandomCrypto, _ := rand.Prime(rand.Reader, 128)
-	return uint(RandomCrypto.Int64()) % uint(len)
-}
-
-func scanNumber() types.Number {
-	var result types.Number
-	for i := 0; i < consts.Digits; i++ {
-		fmt.Scanf("%d", &result[i])
-	}
-	return result
-}
-
-func scanScore(calledNumber types.Number) types.Score {
-	var result types.Score
-	fmt.Scanf("%d", &result.Guessed)
-	fmt.Scanf("%d", &result.Postions)
-	result.Number = calledNumber
-	return result
-}
-
-func youFirst() bool {
-	fmt.Print("Who is first? [0-you]: ")
-	var input int
-	fmt.Scanf("%d", &input)
-	return input == 0
 }
--- game/human_vs_machine/human_vs_machine.go
+++ game/human_vs_machine/human_vs_machine.go
@@ -3,13 +3,57 @@ package human_vs_machine
 import (
 	"fmt"
 
+	"github.com/numbers-game/consts"
+	"github.com/numbers-game/game/common"
 	"github.com/numbers-game/permutations"
+	"github.com/numbers-game/score"
+	"github.com/numbers-game/types"
 )
 
 func Run() {
 	fmt.Println("Human vs Machine mode")
 	numbers := permutations.Generate()
-	for _, number := range numbers {
-		fmt.Println(number)
+	hidden := common.FetchRundomNumber(numbers)
+	youFirst := common.YouFirst()
+	var machineScores types.Scores
+	var machineScore, humanScore types.Score
+	for i := 2; !score.Win(humanScore) || !score.Win(machineScore); i++ {
+		if i%2 == 0 {
+			fmt.Printf("========== %d ==========\n", i/2)
+		}
+		if i%2 == 0 && youFirst || i%2 != 0 && !youFirst {
+			humanScore = humanTurn(hidden)
+		} else {
+			machineScores = machineTurn(numbers, machineScores)
+			numbers = score.Sift(numbers, machineScores)
+			machineScore = machineScores[len(machineScores)-1]
+		}
 	}
+	fmt.Println("=======================")
+}
+
+func humanTurn(hidden types.Number) types.Score {
+	fmt.Print("Human turn: ")
+	human := common.ScanNumber()
+	humanScore := score.Calc(human, hidden)
+	fmt.Printf("Human score: %d-%d\n", humanScore.Guessed, humanScore.Postions)
+	return humanScore
+}
+
+func machineTurn(numbers types.Numbers, machineScores types.Scores) types.Scores {
+	leftNumbers := len(numbers)
+	if leftNumbers == 0 {
+		fmt.Println("Machine turn: No suggestions")
+		return machineScores
+	}
+	fmt.Print("Machine turn: ")
+	calledNumber := common.FetchRundomNumber(numbers)
+	fmt.Print(calledNumber)
+	fmt.Printf(" (%d)\n", leftNumbers)
+	fmt.Print("Machine score: ")
+	if leftNumbers == 1 {
+		fmt.Println(" 4 4")
+		return score.AppendScore(machineScores, types.Score{Number: calledNumber, Guessed: consts.Digits, Postions: consts.Digits})
+	}
+	return score.AppendScore(machineScores, common.ScanScore(calledNumber))
 }
